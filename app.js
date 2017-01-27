efab5e330cf6101d46a391a3ac06a8d435817e4d
davelimber <dave.limber@gmail.com>
2017-01-26 21:26:43 -0700

    several changes to js and formatting

--- app.js
+++ app.js
@@ -9,25 +9,37 @@ function Players(name, position, number) {
 
 }
 
-this.addPlayer = function(name, position, number){
-    var player = new Players(name, position, number
-    //   name: name,
-    //   position: position,
-    //   number: number
-    );
+this.addPlayer = function addPlayer(e) {
+    event.preventDefault();
+    // debugger;
+    var form = event.target
+    var name = form['playerName'].value;
+    var position = form['playerPosition'].value;
+    var number = form['playerNumber'].value;
+
+    var player = new Players(name, position, number);
+
     players.push(player);
-  }
+    drawPlayers();
+}
 
 function getPlayers() {
     return players.slice(0, players.length);
 }
 
 function drawPlayers() {
-    var playerElement = document.getElementsByName('player-card');
+    
+    var playerElement = document.getElementById('player-card');
     playerElement.innerHTML = '';
-     var template = ``;
-     for (player of players) {
-         template +=
-         `${player.name}`
-}
+    var template = ``;
+    var counter = 0;
+    for (player of players) {
+        console.log(player.name);
+        template +=
+        ` <div class="col-xs-2 col-md-2"> <img src="http://s.nflcdn.com/static/content/public/image/fantasy/transparent/200x200/" alt="football player" class="img-thumbnail">
+        <h4><center><em>${player.name}</em></h4><br>
+        <center>${player.position}<br>
+        ${player.number}</center> </div> `
+        counter++;
+    } playerElement.innerHTML += template;
 };
