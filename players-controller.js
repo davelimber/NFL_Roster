8b555e60a864309cdad8d71b6ff3461f5fc80728
davelimber <dave.limber@gmail.com>
2017-01-29 08:34:06 -0700

    several changes to service and controller

--- players-controller.js
+++ players-controller.js
@@ -1,65 +1,64 @@
+
 function PlayersController() {
 
+    // var endpointUri = '';
+    // var callback = '';
 
+    // var pservice = new PlayersService(endpointUri, callback);
 
-// var loading = true; //Start the spinner
-// var apiUrl = "http://api.cbssports.com/fantasy/players/list?version=3.0&SPORT=football&response_format=json";
-// var playerService = new PlayersService(apiUrl, ready);
+    // get a players from SF
 
-// function ready(){
-//     loading = false; //stop the spinner
+    var loading = true; //Start the spinner
+    var apiUrl = "http://api.cbssports.com/fantasy/players/list?version=3.0&SPORT=football&response_format=json";
+    var playerService = new PlayersService(apiUrl, ready);
+    var pservice = playerService;
+    this.ptest = pservice;
 
-//     //Now that all of our player data is back we can safely setup our bindings for the rest of the view.
+    function ready() {
+        loading = false; //stop the spinner
 
-//     $('some-button').on('click',function(){
-//       var teamSF = playerService.getPlayersByTeam("SF");
-//     })
-// };
+        //Now that all of our player data is back we can safely setup our bindings for the rest of the view.
+var playersOnTeam = playerService.getPlayersByPosition("QB");
+console.log(playersOnTeam);
+        // $('some-button').on('click', function () {
+        //     var teamSF = playerService.getPlayersByTeam("SF");
+        // })
+    };
 
+    // this.teamSF =  function () {
+    //     var teamSF = playerService.getPlayersByTeam("SF");
+    //     console.log(teamSF);
+    // };
 
+    this.addPlayer = function addPlayer(e) {
+        event.preventDefault();
 
-var players = [];
+        var form = event.target
 
-function Players(name, position, number) {
-    this.name = name;
-    this.position = position;
-    this.number = number;
+        var name = form['playerName'].value;
+        var position = form['playerPosition'].value;
+        var number = form['playerNumber'].value;
 
-}
+        pservice.addNewPlayer(name, position, number);
+        drawPlayers();
+    }
 
-this.addPlayer = function addPlayer(e) {
-    event.preventDefault();
-    debugger;
-    var form = event.target
-    var name = form['playerName'].value;
-    var position = form['playerPosition'].value;
-    var number = form['playerNumber'].value;
+    function drawPlayers() {
 
-    var player = new Players(name, position, number);
-
-    players.push(player);
-    drawPlayers();
-}
-
-function getPlayers() {
-    return players.slice(0, players.length);
-}
-
-function drawPlayers() {
-    
-    var playerElement = document.getElementById('player-card');
-    playerElement.innerHTML = '';
-    var template = ``;
-    var counter = 0;
-    for (player of players) {
-        console.log(player.name);
-        template +=
-        ` <div class="col-xs-12 col-md-2"> <img src="http://s.nflcdn.com/static/content/public/image/fantasy/transparent/200x200/" alt="football player" class="img">
+        var playerElement = document.getElementById('player-card');
+        playerElement.innerHTML = '';
+        var template = ``;
+        var counter = 0;
+        // var players = pservice.getPlayers();
+        for (player of pservice.getPlayers()) {
+            console.log(player.name);
+            template +=
+                ` <div class="col-xs-12 col-md-2"> <img src="http://s.nflcdn.com/static/content/public/image/fantasy/transparent/200x200/" alt="football player" class="img">
         <h4><center><em>${player.name}</em></h4><br>
         <center>${player.position}<br>
         ${player.number}</center> </div> `
-        counter++;
-    } playerElement.innerHTML += template;
-};
+            counter++;
+        } playerElement.innerHTML += template;
+    };
 
 };
--- players-service.js
+++ players-service.js
@@ -1,48 +1,118 @@
-var PlayersService = function(endpointUri, callback){
-//     var playersData = [];
+// function PlayersService() {
 
-//     this.getPlayersByTeam = function(teamName){
-//         playersData.filter(function(player){
-//           if(player.team == teamName){
-//             return true;
-//         };
-//     });
-//     }
-//     this.getPlayersByPosition = function(position){
-//         playersData.filter(function(player){
-//           if(player.position == position){
-//             return true;
-//         };
-//         });
+function PlayersService(endpointUri, callback) {
+    var playersData = [];
+    returnedData = [{ name: "test" }]
 
-//     }
+    this.getPlayersByTeam = function (teamName) {
+        // var filteredPlayers = [];
+        // // console.log(playersData[0]);
+        // for (var i = 0; i < playersData.length; i++) {
+        //     if (playersData[i].pro_team === teamName) {
+        //         filteredPlayers.push = playersData[i].lastname;
+        //     }
+        //        console.log(filteredPlayers.length);
+        // }
 
-// function loadPlayersData(){
+        // // console.log(teamName);
+        playersData.filter(function (player) {
+            // console.log(player.pro_team);
+            if (playersData.pro_team === teamName) {
+                return true;
+            }; console.log('finished');
+        });
+    }
 
-//       //Lets check the localstorage for the data before making the call.
-//       //Ideally if a user has already used your site 
-//       //we can cut down on the load time by saving and pulling from localstorage 
-   
-//       var localData = localStorage.getItem('playerData');
-//       if(localData){
-    
-//         playersData = JSON.parse(localData);
-//         return callback(); 
-//         //return will short-circuit the loadPlayersData function
-//         //this will prevent the code below from ever executing
-//       }
-      
-//       var url = "http://bcw-getter.herokuapp.com/?url=";
-//       var apiUrl = url + encodeURIComponent(endpointUri);
+    this.getPlayersByPosition = function (position) {
+        playersData.filter(function (player) {
+            if (player.position == position) {
+                return true;
+            }
+        });
+    }
 
-//         $.getJSON(apiUrl, function(data){
-//           playersData = data.body.players;
-//           console.log('Player Data Ready')
-//           console.log('Writing Player Data to localStorage')
-//           localStorage.setItem('playersData', JSON.stringify(playersData))
-//           console.log('Finished Writing Player Data to localStorage')
-//           callback()
-//         });
-//     }  
-// loadPlayersData(); //call the function above every time we create a new service
+    //     this.getPlayersByPosition = function (position) {
+    //         var filteredPositions = [];
+    //         counter=0;
+    //         playersData.filter(function (player) {
+    // // con
+    // if (player.position === position) {
+    //     filteredPositions.push = player.lastname;
+    //     // console.log(counter);
+    //     counter++;
+    // } console.log(counter);
+    //             // if (player.position === position) {
+    //             //     return true;
+    //             // };
+    //         });
+
+    //     }
+
+    // function teamSF() {
+    //     var teamSF = getPlayersByPosition("D");
+    //     // console.log(teamSF);
+    // };
+
+
+
+
+    function loadPlayersData() {
+
+        //Lets check the localstorage for the data before making the call.
+        //Ideally if a user has already used your site 
+        //we can cut down on the load time by saving and pulling from localstorage 
+
+        var localData = localStorage.getItem('playerData');
+        if (localData) {
+
+            playersData = JSON.parse(localData);
+            return callback();
+            //return will short-circuit the loadPlayersData function
+            //this will prevent the code below from ever executing
+        }
+
+        var url = "http://bcw-getter.herokuapp.com/?url=";
+        var apiUrl = url + encodeURIComponent(endpointUri);
+
+        $.getJSON(apiUrl, function (data) {
+            playersData = data.body.players;
+            console.log('Player Data Ready')
+            console.log('Writing Player Data to localStorage')
+            localStorage.setItem('playersData', JSON.stringify(playersData))
+            console.log('Finished Writing Player Data to localStorage')
+            // var retrobj = localStorage.getItem('playersData');
+            // console.log("retreobj: ", JSON.parse(retrobj));
+            //    teamSF();
+            // console.log(localStorage.playersData.lastname)
+
+            //   getPlayersByTeam("SF");
+
+            callback()
+        });
+    }
+    loadPlayersData(); //call the function above every time we create a new service
+
+    var players = [];
+
+    function Players(name, position, number) {
+        this.name = name;
+        this.position = position;
+        this.number = number;
+
+    }
+
+    this.addNewPlayer = function (name, position, number) {
+        var player = new Players(name, position, number);
+        players.push(player);
+    }
+
+
+    this.getPlayers = function getPlayers() {
+        // this.playerArray = players;
+        // console.log(this.playerArray);
+        // return this.playerArray.slice(0, players.length);
+        return players.slice(0, players.length);
+    }
+
+
 } 
