0c59773b0f5b6a2c9687e80fc26af787b29785be
davelimber <dave.limber@gmail.com>
2017-01-29 18:57:59 -0700

    commented out position lookup

--- players-controller.js
+++ players-controller.js
@@ -65,34 +65,36 @@ function PlayersController() {
         } playerElement.innerHTML += template;
     };
 
-    this.getQB = function getQB() {
-        var playersOnTeam = pservice.getPlayersByPosition("QB");
-        // console.log(playersOnTeam);
-    }
+// Uncomment section to activate
 
-    this.showPlayers = function showPlayers(e) {
-        event.preventDefault();
-debugger
-        var form = event.target
-        var position = form['positionRequested'].value;
-        var playersOnTeam = pservice.
+//     this.getQB = function getQB() {
+//         var playersOnTeam = pservice.getPlayersByPosition("QB");
+//         // console.log(playersOnTeam);
+//     }
 
-        var playerElement = document.getElementById('player-card');
-        playerElement.innerHTML = '';
-        var template = ``;
-        var counter = 0;
+//     this.showPlayers = function showPlayers(e) {
+//         event.preventDefault();
+// debugger
+//         var form = event.target
+//         var position = form['positionRequested'].value;
+//         var playersOnTeam = pservice.
 
-        for (player of playersOnTeam) {
-            console.log(player);
-            template +=
-                `<div class="player-card"> 
-                <img src=${player.photo} alt="football player" class="img">
-                <h4><center>${player.name}</h4>
-                <h4>${player.position}</h4>
-                <h4>${player.number}</h4> 
-                </div>`
-            counter++;
-        } playerElement.innerHTML += template;
+//         var playerElement = document.getElementById('player-card');
+//         playerElement.innerHTML = '';
+//         var template = ``;
+//         var counter = 0;
+
+//         for (player of playersOnTeam) {
+//             console.log(player);
+//             template +=
+//                 `<div class="player-card"> 
+//                 <img src=${player.photo} alt="football player" class="img">
+//                 <h4><center>${player.name}</h4>
+//                 <h4>${player.position}</h4>
+//                 <h4>${player.number}</h4> 
+//                 </div>`
+//             counter++;
+//         } playerElement.innerHTML += template;
         
-    }
+//     }
 };
